0df0ef0d43f65e451f3ed2dbc039fe7fbd40a782
larsboye <lars.thomas.boye@tellu.no>
2018-09-17 09:50:45 +0200

    Baseline clover reports for the three Tellu usecase projects.

---
+++ tellu/filterstore/clover-baseline-20180914/treemap-dash-json.js
@@ -0,0 +1,24 @@
+var treeMapJson = {"id":"Clover database fr sep 14 2018 15:04:22 CEST0","name":"","data":{
+    "$area":4137.0,"$color":35.750546,"title":
+    " 4137 Elements, 35,8% Coverage"},"children":[{"id":
+      "no.tellu.cloud.fs.itest4230","name":"no.tellu.cloud.fs.itest","data":{
+        "$area":12.0,"$color":25.0,"title":
+        "no.tellu.cloud.fs.itest 12 Elements, 25% Coverage"},"children":[]},{
+      "id":"no.tellu.cloud.filterstore0","name":
+      "no.tellu.cloud.filterstore","data":{"$area":340.0,"$color":43.82353,
+        "title":"no.tellu.cloud.filterstore 340 Elements, 43,8% Coverage"},
+      "children":[]},{"id":"no.tellu.cloud.filterstore.cmd340","name":
+      "no.tellu.cloud.filterstore.cmd","data":{"$area":713.0,"$color":
+        53.155678,"title":
+        "no.tellu.cloud.filterstore.cmd 713 Elements, 53,2% Coverage"},
+      "children":[]},{"id":"no.tellu.cloud.filterstore.devicecommand1053",
+      "name":"no.tellu.cloud.filterstore.devicecommand","data":{"$area":
+        344.0,"$color":0.8720931,"title":
+        "no.tellu.cloud.filterstore.devicecommand 344 Elements, 0,9% Coverage"},
+      "children":[]},{"id":"no.tellu.cloud.filterstore.devicefilter1397",
+      "name":"no.tellu.cloud.filterstore.devicefilter","data":{"$area":
+        2728.0,"$color":34.640762,"title":
+        "no.tellu.cloud.filterstore.devicefilter 2728 Elements, 34,6% Coverage"},
+      "children":[]}]}
+;
+processTreeMapDashJson (treeMapJson);
